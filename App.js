1e11a235b34c49c55228fab01b41ec0f73ccc304
Saket789 <56218641+Saket789@users.noreply.github.com>
2020-10-02 16:45:47 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+// https://medium.com/@dan_abramov/smart-and-dumb-components-7ca2f9a7c7d0
+
+import React, {Component} from "react"
+import Form from "./FormContainer.js"
+
+function App() {
+    return (
+        <Form />
+        // <h1>saket vajpai</h1>
+    )
+}
+
+export default App
